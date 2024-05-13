d6a0b63fe01705a8d4d3b03dbc5e38f89c342443
Jakhongir Rakhmonov <jrahmonov2@gmail.com>
2024-05-13 19:57:41 +0200

    neo-tree

--- lua/jakhon/lazy/neo-tree.lua
+++ lua/jakhon/lazy/neo-tree.lua
@@ -7,4 +7,13 @@ return {
         "MunifTanjim/nui.nvim",
         -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
     },
+    config = function()
+        require("neo-tree").setup({
+            filesystem = {
+                filtered_items = {
+                    visible = true
+                }
+            }
+        })
+    end
 }
