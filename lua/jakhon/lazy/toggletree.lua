614ea746bfde1fb850a56227122db798d7fb2ad6
Jakhongir Rakhmonov <jakhon@amazon.com>
2024-05-14 20:44:03 +0000

    add toggleterm

---
+++ lua/jakhon/lazy/toggletree.lua
@@ -0,0 +1,13 @@
+return {
+    {
+        'akinsho/toggleterm.nvim',
+        version = "*",
+        opts = {
+            direction = "float",
+            open_mapping = [[<c-\>]],
+            float_opts = {
+                border = "curved"
+            }
+        }
+    }
+}
