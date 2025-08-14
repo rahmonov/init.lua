65136924b28c6a9728e1cef8defacf1b9e5d71f0
Jakhongir Rakhmonov <jrahmonov2@gmail.com>
2025-08-14 11:38:45 +0200

    Rename tsserver to ts_ls

--- lua/jakhon/lazy/lsp.lua
+++ lua/jakhon/lazy/lsp.lua
@@ -27,7 +27,7 @@ return {
         require("mason-lspconfig").setup({
             ensure_installed = {
                 "lua_ls",
-                "tsserver",
+                "ts_ls",
                 "clangd",
                 "pyright",
                 "jsonls",
